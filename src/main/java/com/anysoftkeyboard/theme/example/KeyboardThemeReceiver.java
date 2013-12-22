c588cf97266f835ca6ce0af4f45b3fe40e01edae
Menny Even Danan <menny@evendanan.net>
2013-12-21 20:36:57 -0500

    initial commit for the base sample Theme add-on. This is based on the ICS theme.

---
+++ src/main/java/com/anysoftkeyboard/theme/example/KeyboardThemeReceiver.java
@@ -0,0 +1,17 @@
+package com.anysoftkeyboard.theme.example;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Although this is an empty class, it must exists. This is how AnySoftKeyboard "finds" your theme.
+ */
+public class KeyboardThemeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}
